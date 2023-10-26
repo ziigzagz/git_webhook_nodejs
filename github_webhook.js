b14091532631e499614c7303ed220350a4f1d3aa
ziigzagz <iffan.h@ideabranch.co.th>
2023-10-26 23:31:04 +0700

    submit

--- github_webhook.js
+++ github_webhook.js
@@ -1,30 +1,43 @@
-var http = require('http')
-var createHandler = require('github-webhook-handler')
-const { exec } = require('child_process');
-var handler = createHandler({ path: '/webhook', secret: 'myhashsecret' })
- 
-http.createServer(function (req, res) {
-  handler(req, res, function (err) {
-    res.statusCode = 404
-    res.end('no such location')
+var http = require("http");
+var createHandler = require("github-webhook-handler");
+const { exec } = require("child_process");
+var handler = createHandler({ path: "/webhook", secret: "myhashsecret" });
+
+http
+  .createServer(function (req, res) {
+    handler(req, res, function (err) {
+      res.statusCode = 404;
+      res.end("no such location");
+    });
   })
-}).listen(8008)
- 
-handler.on('error', function (err) {
-  console.error('Error:', err.message)
-})
- 
-handler.on('push', function (event) {
-  console.log('Received a push event for %s to %s',
+  .listen(8008);
+
+handler.on("error", function (err) {
+  console.error("Error:", err.message);
+});
+
+handler.on("push", function (event) {
+  console.log(
+    "Received a push event for %s to %s",
     event.payload.repository.name,
-    event.payload.ref)
-    exec('sh cmd.sh')
-})
- 
-handler.on('issues', function (event) {
-  console.log('Received an issue event for %s action=%s: #%d %s',
+    event.payload.ref
+  );
+
+  var yourscript = exec("sh cmd.sh", (error, stdout, stderr) => {
+    console.log(stdout);
+    console.log(stderr);
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    }
+  });
+});
+
+handler.on("issues", function (event) {
+  console.log(
+    "Received an issue event for %s action=%s: #%d %s",
     event.payload.repository.name,
     event.payload.action,
     event.payload.issue.number,
-    event.payload.issue.title)
-})
+    event.payload.issue.title
+  );
+});
