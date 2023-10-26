0887ed0214c1b4a5bb61fab372cd665248013146
ziigzagz <iffan.h@ideabranch.co.th>
2023-10-26 23:46:52 +0700

    update command shell

--- cmd.sh
+++ cmd.sh
@@ -3,7 +3,12 @@ sleep 3
 cd ..
 cd umayplus-front-end/
 docker compose down
-echo "down..."
+echo "Down..."
 sleep 10
+docker rmi $(docker images 'umayplus-front-end-umayplus-website' -q)
+echo "Remove Docker images..."
+sleep 5
+docker compose build --no-cache
+sleep 30
 docker compose up -d
 echo "up..."
