66cc9f942c227bf822381aa2d299178bee961f47
ziigzagz <iffan.h@ideabranch.co.th>
2023-11-02 22:07:16 +0700

    update

--- cmd.sh
+++ cmd.sh
@@ -4,8 +4,8 @@ sleep 3
 cd /home/umayplus/umayplus-front-end
 sleep 1
 
-git checkout del-split-2
-echo "checkout del-split-2"
+git checkout dev-split-2
+echo "checkout dev-split-2"
 sleep 1
 
 git pull
@@ -15,14 +15,30 @@ docker compose down
 echo "Down..."
 sleep 5
 
-docker images -a | grep "umayplus-front-end-umayplus-website" | awk '{print $3}' | xargs docker rmi
+docker system prune --all
+echo "Prune..."
+sleep 3
+
+sudo rm -R /var/lib/docker/overlay2
+sleep 10
+
+sudo rm -R /var/lib/docker/image
+sleep 1
+
+docker system prune -f
+sleep 5
+
+systemctl restart docker
+sleep 3
+
+docker images -a | grep "umayplus-front-end-umayplus-website" | awk '{print $3}' | xargs docker rmi -f
 sleep 1
 
 echo "Remove Docker images..."
-sleep 5
+sleep 1
 
 docker compose build --no-cache
-sleep 60
+sleep 45
 
 docker compose up -d
 echo "up..."
